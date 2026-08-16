47ce51b3d61bc99a6f2be65dfc4152fc4d1b3961
memetreza34-ux <memetreza34@gmail.com>
2026-08-16 13:02:20 +0200

    feat: guard GuardAI bootstrap with error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './App.tsx'
-import { ThemeProvider } from './components/ThemeProvider'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { App } from './App.tsx';
+import { AppErrorBoundary } from './components/AppErrorBoundary';
+import { ThemeProvider } from './components/ThemeProvider';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('GuardAI root element is missing.');
+}
+
+createRoot(rootElement).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <AppErrorBoundary>
+      <ThemeProvider defaultTheme="dark" storageKey="guardai-ui-theme">
+        <App />
+      </ThemeProvider>
+    </AppErrorBoundary>
   </StrictMode>,
-)
+);
