84a8e2528347b0c0486532decbf9b8d9909c2c1d
memetreza34-ux <memetreza34@gmail.com>
2026-08-16 18:32:38 +0200

    feat: route public Trust links before product app bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App.tsx';
 import { AppErrorBoundary } from './components/AppErrorBoundary';
+import PublicTrustPage from './components/PublicTrustPage';
 import { ThemeProvider } from './components/ThemeProvider';
 
 const rootElement = document.getElementById('root');
@@ -11,11 +12,16 @@ if (!rootElement) {
   throw new Error('GuardAI root element is missing.');
 }
 
+const publicTrustMatch = window.location.pathname.match(/^\/trust\/([A-Za-z0-9_-]{24,80})\/?$/);
+const rootView = publicTrustMatch
+  ? <PublicTrustPage publicSlug={publicTrustMatch[1]} />
+  : <App />;
+
 createRoot(rootElement).render(
   <StrictMode>
     <AppErrorBoundary>
       <ThemeProvider defaultTheme="dark" storageKey="guardai-ui-theme">
-        <App />
+        {rootView}
       </ThemeProvider>
     </AppErrorBoundary>
   </StrictMode>,
