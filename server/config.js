aa51b1c9a677204a21a6898aa61f4fc84dcd3b6e
memetreza34-ux <memetreza34@gmail.com>
2026-08-16 18:39:46 +0200

    feat: add Stripe billing runtime configuration

--- server/config.js
+++ server/config.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+const {
+  normalizeBillingProvider,
+  parseStripePlanPriceMap,
+} = require('./domain/billingConfig');
+
 const config = Object.freeze({
   port: Number(process.env.PORT || 3001),
   corsOrigins: (process.env.CORS_ORIGIN || 'http://localhost:5173')
@@ -15,6 +20,11 @@ const config = Object.freeze({
   databasePoolMax: Number(process.env.DATABASE_POOL_MAX || 10),
   workerLeaseSeconds: Number(process.env.WORKER_LEASE_SECONDS || 60),
   workerPollMs: Number(process.env.WORKER_POLL_MS || 2000),
+  billingProvider: normalizeBillingProvider(process.env.BILLING_PROVIDER),
+  publicAppUrl: process.env.PUBLIC_APP_URL || 'http://localhost:5173',
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY || '',
+  stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET || '',
+  stripePlanPriceMap: parseStripePlanPriceMap(process.env.STRIPE_PLAN_PRICE_MAP_JSON || ''),
   maxUploadBytes: 10 * 1024 * 1024,
   maxHtmlBytes: 2 * 1024 * 1024,
   maxExtractedTextChars: 30000,
