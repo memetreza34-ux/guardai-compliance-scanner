a8558f99a9714faf494e185d062174286c59da57
memetreza34-ux <memetreza34@gmail.com>
2026-08-18 19:42:16 +0200

    feat: mount public contact and GitHub callback pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import { App } from './App.tsx';
 import { AppErrorBoundary } from './components/AppErrorBoundary';
 import BillingReturnPage from './components/BillingReturnPage';
+import GitHubIntegrationCallbackPage from './components/GitHubIntegrationCallbackPage';
+import PublicContactPage from './components/PublicContactPage';
 import PublicTrustPage from './components/PublicTrustPage';
 import { ThemeProvider } from './components/ThemeProvider';
 
@@ -15,11 +17,18 @@ if (!rootElement) {
 
 const publicTrustMatch = window.location.pathname.match(/^\/trust\/([A-Za-z0-9_-]{24,80})\/?$/);
 const billingReturn = /^\/billing\/return\/?$/.test(window.location.pathname);
+const publicContact = /^\/contact\/?$/.test(window.location.pathname);
+const githubCallback = /^\/integrations\/github\/callback\/?$/.test(window.location.pathname);
+
 const rootView = publicTrustMatch
   ? <PublicTrustPage publicSlug={publicTrustMatch[1]} />
   : billingReturn
     ? <BillingReturnPage />
-    : <App />;
+    : publicContact
+      ? <PublicContactPage />
+      : githubCallback
+        ? <GitHubIntegrationCallbackPage />
+        : <App />;
 
 createRoot(rootElement).render(
   <StrictMode>
